166d260db320e473da7a98307e9ef1649e107d80
Nane <nane_arshakyan@edu.aua.am>
2018-09-13 21:26:29 +0300

    Homework2

---
+++ homework2.js
@@ -0,0 +1,25 @@
+alert ("hello dear customer");
+
+ const femaleList = [ 
+ [{name:"Nane"},
+  {age:18 }],
+ [{name:"Mane"}, 
+ {age:25}],
+ [{name:"Karine"},
+  {age:38}]
+ ];
+
+ const maleList = [
+ [{name:"Karen"},
+ {age:18}],
+ [{name:"Paruyr"},
+ {age:20}],
+ [{name:"Armen"},
+ {age:28}]
+ ];
+ 
+if(maleList[0].age - femaleList[0].age === 2)
+  {console.log ('You are born for each other')}
+   else {console.log ('You will find someone else')};
+
+  
